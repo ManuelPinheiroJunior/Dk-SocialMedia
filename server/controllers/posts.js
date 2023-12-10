cb93af3985b23183df918628d161b1f2e8a4cfb7
Manuel Junior <manuel.jr@outlook.com>
2023-12-10 23:40:37 +0000

    Feat: Alter data

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,12 +4,12 @@ import User from "../models/User.js";
 /* CREATE */
 export const createPost = async (req, res) => {
   try {
-    const { userId, description, picturePath } = req.body;
+    const { userId, description, picturePath, firstName, lastName } = req.body;
     const user = await User.findById(userId);
     const newPost = new Post({
       userId,
-      firstName: user.firstName,
-      lastName: user.lastName,
+      firstName: firstName,
+      lastName: lastName,
       location: user.location,
       description,
       userPicturePath: user.picturePath,
